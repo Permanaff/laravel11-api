5ba4e16396886312ea224203a9e20da396121754
Permanaff <fajar.permanaf314@gmail.com>
2024-07-09 15:30:51 +0700

    Menampilkan Data Dari Database

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //
+    public function index() 
+    {
+        $posts = Post::latest()->paginate(5);
+
+        return new PostResource(true, 'List Data Post', $posts);
+    }
+}
