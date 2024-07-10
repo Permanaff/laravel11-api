3cc53dbdc294487f2febf53e717da05c8ba3df0b
Permanaff <fajar.permanaf314@gmail.com>
2024-07-10 11:08:26 +0700

    Menambahkan Insert Data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -15,5 +16,34 @@ class PostController extends Controller
         $posts = Post::latest()->paginate(5);
 
         return new PostResource(true, 'List Data Post', $posts);
+    }
+
+    public function store(Request $request)
+    {
+        // Definisi aturan validasi 
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg, png, gif, svg|max:2048',
+            'title' => 'require',
+            'content' => 'require',
+
+        ]);
+
+        // mengecek validasi jika fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // upload imgae 
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        // create post 
+        $post = Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title, 
+            'content' => $request->content
+        ]);
+
+        return new PostResource(true, "Data Post Berhasil Ditambahkan!", $post);
     }
 }
